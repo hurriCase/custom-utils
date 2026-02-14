edc44ed426494e89a735071ed2b370bfaab2ecc2
hurriCase <dmitriy.pastanovskiy@gmail.com>
2026-02-14 13:03:30 +0300

    [feat] custom editor for embdedded editor cases

--- Editor/Scripts/Localization/KeyDrawer/LocalizationKeyDrawer.cs
+++ Editor/Scripts/Localization/KeyDrawer/LocalizationKeyDrawer.cs
@@ -1,4 +1,5 @@
 ﻿using CustomUtils.Editor.Scripts.Extensions;
+using CustomUtils.Editor.Scripts.Localization.LocalizationSelector;
 using CustomUtils.Runtime.Localization;
 using UnityEditor;
 using UnityEngine;
@@ -19,6 +20,26 @@ namespace CustomUtils.Editor.Scripts.Localization.KeyDrawer
             rootVisualElement.styleSheets.Add(_unityFieldStyle);
 
             return rootVisualElement;
+        }
+
+        // Simplified version for embedded editor cases
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var guidProperty = property.FindFieldRelative(nameof(LocalizationKey.GUID));
+            var currentGuid = guidProperty.stringValue;
+
+            var rect = EditorGUI.PrefixLabel(position, label);
+
+            var currentKey = LocalizationRegistry.Instance.Entries.TryGetValue(currentGuid, out var entry)
+                ? entry.Key
+                : "[None]";
+
+            if (GUI.Button(rect, currentKey, EditorStyles.popup))
+                LocalizationSelectorWindow.ShowWindow(entry, selectedEntry =>
+                {
+                    guidProperty.stringValue = selectedEntry.GUID;
+                    guidProperty.serializedObject.ApplyModifiedProperties();
+                });
         }
     }
 }
