7cffbca70c3e0efa09d0bd7b78e475c3c064e9ee
hurriCase <dmitriy.pastanovskiy@gmail.com>
2026-02-19 10:34:08 +0300

    [feat] LanguageLocalizationConfig.cs

---
+++ Runtime/Localization/LanguageLocalizationConfig.cs
@@ -0,0 +1,20 @@
+﻿using CustomUtils.Runtime.CustomTypes.Collections;
+using CustomUtils.Runtime.CustomTypes.Singletons;
+using CustomUtils.Unsafe;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace CustomUtils.Runtime.Localization
+{
+    [PublicAPI]
+    public sealed class LanguageLocalizationConfig : SingletonScriptableObject<LanguageLocalizationConfig>
+    {
+        [SerializeField] private EnumArray<SystemLanguage, string> _localizations;
+
+        public string GetLocalization(SystemLanguage currentEnum)
+        {
+            var enumValue = UnsafeEnumConverter<SystemLanguage>.ToInt32(currentEnum);
+            return _localizations[enumValue];
+        }
+    }
+}
