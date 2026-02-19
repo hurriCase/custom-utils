183b62d14120daaf71e25a904ccf2bdd29496a5d
hurriCase <dmitriy.pastanovskiy@gmail.com>
2026-02-19 17:49:33 +0300

    [refactor] VisibilityHandler.cs multi animation support

--- Runtime/UI/Windows/VisibilityHandler.cs
+++ Runtime/UI/Windows/VisibilityHandler.cs
@@ -1,7 +1,8 @@
-﻿using CustomUtils.Runtime.Animations.Base;
-using CustomUtils.Runtime.Extensions;
+﻿using System.Collections.Generic;
+using CustomUtils.Runtime.Animations.Base;
 using Cysharp.Threading.Tasks;
 using JetBrains.Annotations;
+using PrimeTween;
 using UnityEngine;
 
 namespace CustomUtils.Runtime.UI.Windows
@@ -9,29 +10,28 @@ namespace CustomUtils.Runtime.UI.Windows
     [PublicAPI]
     public class VisibilityHandler : MonoBehaviour
     {
-        [SerializeField] private CanvasGroup _canvasGroup;
+        [SerializeReference, SerializeReferenceDropdown] private List<IAnimation<VisibilityState>> _visibilityAnimations;
 
-        [SerializeReference, SerializeReferenceDropdown] private IAnimation<VisibilityState> _visibilityAnimation;
+        public virtual async UniTask ShowAsync() => await CreateVisibilitySequence(VisibilityState.Visible);
 
-        public virtual async UniTask ShowAsync()
-        {
-            _canvasGroup.Show();
-
-            await _visibilityAnimation.PlayAnimation(VisibilityState.Visible);
-        }
-
-        public virtual async UniTask HideAsync()
-        {
-            await _visibilityAnimation.PlayAnimation(VisibilityState.Hidden);
-
-            _canvasGroup.Hide();
-        }
+        public virtual async UniTask HideAsync() => await CreateVisibilitySequence(VisibilityState.Hidden);
 
         public void HideImmediately()
         {
-            _visibilityAnimation.PlayAnimation(VisibilityState.Hidden, true);
+            foreach (var animation1 in _visibilityAnimations)
+                animation1.PlayAnimation(VisibilityState.Hidden, true);
+        }
 
-            _canvasGroup.Hide();
+        private async UniTask CreateVisibilitySequence(VisibilityState visibilityState)
+        {
+            var sequence = Sequence.Create();
+            foreach (var visibilityAnimation in _visibilityAnimations)
+            {
+                var playAnimation = visibilityAnimation.PlayAnimation(visibilityState);
+                sequence.Group(playAnimation);
+            }
+
+            await sequence;
         }
     }
 }
