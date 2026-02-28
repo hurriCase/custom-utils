b2a037cb4aec89ca04ced61de4dadbc591edf7d7
hurriCase <dmitriy.pastanovskiy@gmail.com>
2026-02-28 19:49:20 +0300

    [feat] SpriteRendererColorAnimation.cs

---
+++ Runtime/Animations/Settings/ColorAnimationSettings.cs
@@ -0,0 +1,11 @@
+﻿using CustomUtils.Runtime.Animations.Base;
+using UnityEngine;
+
+namespace CustomUtils.Runtime.Animations.Settings
+{
+    [CreateAssetMenu(
+        fileName = nameof(ColorAnimationSettings),
+        menuName = AnimationSettingsPath + nameof(ColorAnimationSettings)
+    )]
+    public sealed class ColorAnimationSettings : AnimationSettings<Color> { }
+}
---
+++ Runtime/Animations/SpriteRendererColorAnimation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CustomUtils.Runtime.Animations.Base;
+using CustomUtils.Runtime.Animations.Settings;
+using JetBrains.Annotations;
+using PrimeTween;
+using UnityEngine;
+
+namespace CustomUtils.Runtime.Animations
+{
+    /// <summary>
+    /// Animates the color of a SpriteRenderer based on state.
+    /// </summary>
+    /// <typeparam name="TState">The enum type representing animation states.</typeparam>
+    [PublicAPI]
+    [Serializable]
+    public sealed class SpriteRendererColorAnimation<TState> : AnimationBase<TState, Color, ColorAnimationSettings>
+        where TState : unmanaged, Enum
+    {
+        [SerializeField] private SpriteRenderer _target;
+
+        protected override void SetValueInstant(Color color)
+        {
+            _target.color = color;
+        }
+
+        protected override Tween CreateTween(ColorAnimationSettings animationSettings)
+            => Tween.Color(_target, animationSettings.TweenSettings);
+    }
+}
