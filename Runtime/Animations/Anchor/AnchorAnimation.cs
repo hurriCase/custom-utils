39046e281acbae5bb3058c8c5bedf8a780da4ea5
hurriCase <dmitriy.pastanovskiy@gmail.com>
2026-05-26 19:41:59 +0300

    feat: AnchorAnimation

---
+++ Runtime/Animations/Anchor/AnchorAnimation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using CustomUtils.Runtime.Animations.Base;
+using CustomUtils.Runtime.Animations.Settings;
+using JetBrains.Annotations;
+using PrimeTween;
+using UnityEngine;
+
+namespace CustomUtils.Runtime.Animations.Anchor
+{
+    /// <summary>
+    /// Animates the anchorMin and anchorMax of a RectTransform based on state.
+    /// xy = anchorMin, zw = anchorMax.
+    /// </summary>
+    /// <typeparam name="TState">The enum type representing animation states.</typeparam>
+    [PublicAPI]
+    [Serializable]
+    public sealed class AnchorAnimation<TState> : AnimationBase<TState, Vector4, Vector4AnimationSettings>
+        where TState : unmanaged, Enum
+    {
+        [SerializeField] private RectTransform _target;
+
+        protected override void SetValueInstant(Vector4 value)
+        {
+            _target.anchorMin = new Vector2(value.x, value.y);
+            _target.anchorMax = new Vector2(value.z, value.w);
+        }
+
+        protected override Tween CreateTween(Vector4AnimationSettings animationSettings)
+            => Tween.Custom(
+                this,
+                animationSettings.TweenSettings,
+                static (self, value) => self.SetValueInstant(value));
+    }
+}
---
+++ Runtime/Animations/Settings/Vector4AnimationSettings.cs
@@ -0,0 +1,11 @@
+﻿using CustomUtils.Runtime.Animations.Base.Settings;
+using UnityEngine;
+
+namespace CustomUtils.Runtime.Animations.Settings
+{
+    [CreateAssetMenu(
+        fileName = nameof(Vector4AnimationSettings),
+        menuName = AnimationSettingsPath + nameof(Vector4AnimationSettings)
+    )]
+    public sealed class Vector4AnimationSettings : TweenAnimationSettingsBase<Vector4> { }
+}
