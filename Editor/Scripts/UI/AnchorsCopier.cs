2dde261b6d09e8ae2b5d4a1f97e78ae1d37f8377
hurriCase <dmitriy.pastanovskiy@gmail.com>
2026-05-27 18:35:15 +0300

    feat: AnchorsCopier

---
+++ Editor/Scripts/UI/AnchorsCopier.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace CustomUtils.Editor.Scripts.UI
+{
+    internal static class AnchorsCopier
+    {
+        private const string RectTransformContextMenuPath = "CONTEXT/RectTransform/";
+
+        [MenuItem(RectTransformContextMenuPath + nameof(CopyAnchorsAsVector4))]
+        private static void CopyAnchorsAsVector4(MenuCommand command)
+        {
+            var rectTransform = (RectTransform)command.context;
+            var min = rectTransform.anchorMin;
+            var max = rectTransform.anchorMax;
+            EditorGUIUtility.systemCopyBuffer = $"Vector4({min.x}, {min.y}, {max.x}, {max.y})";
+        }
+    }
+}
