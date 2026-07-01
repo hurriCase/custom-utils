6d90bcce3bf06cc207bcd5c8964a59d8fb7c64b1
hurriCase <dmitriy.pastanovskiy@gmail.com>
2026-07-01 17:57:11 +0300

    feat: Dictionary instead of SerializedDictionary for 6.7 Unity

--- Runtime/UI/Theme/ColorModifiers/GradientModifier/ShaderGradient/GradientConfig.cs
+++ Runtime/UI/Theme/ColorModifiers/GradientModifier/ShaderGradient/GradientConfig.cs
@@ -1,13 +1,23 @@
-﻿using AYellowpaper.SerializedCollections;
-using CustomUtils.Runtime.AssetLoader;
+﻿using CustomUtils.Runtime.AssetLoader;
 using CustomUtils.Runtime.CustomTypes.Singletons;
 using UnityEngine;
+
+#if UNITY_6000_7_OR_NEWER
+using System.Collections.Generic;
+
+#else
+using AYellowpaper.SerializedCollections;
+#endif
 
 namespace CustomUtils.Runtime.UI.Theme.ColorModifiers.GradientModifier.ShaderGradient
 {
     [Resource(name: nameof(GradientConfig))]
     internal sealed class GradientConfig : SingletonScriptableObject<GradientConfig>
     {
+#if UNITY_6000_7_OR_NEWER
+        [field: SerializeField] internal Dictionary<GradientType, string> GradientKeywords { get; set; }
+#else
         [field: SerializeField] internal SerializedDictionary<GradientType, string> GradientKeywords { get; set; }
+#endif
     }
 }
