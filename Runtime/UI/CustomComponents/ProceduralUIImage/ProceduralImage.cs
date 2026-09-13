f5c54a2baa515cd4ed98baa9e50285b16e784b32
hurriCase <dmitriy.pastanovskiy@gmail.com>
2026-09-13 15:15:41 +0300

    Revert "feat: ProceduralImageGlow.cs"
    This reverts commit 3a2af08801859d6dc9c99bc973c6b1ee18bcad29.

--- Runtime/UI/CustomComponents/ProceduralUIImage/ProceduralImage.cs
+++ Runtime/UI/CustomComponents/ProceduralUIImage/ProceduralImage.cs
@@ -58,7 +58,7 @@ namespace CustomUtils.Runtime.UI.CustomComponents.ProceduralUIImage
             set => base.material = value;
         }
 
-#if UNITY_EDITOR
+#if UNITY_EDITOR || DEVELOPMENT_BUILD
         private static readonly ProfilerMarker _markerEncodeVertices
             = new(ProfilerCategory.Render, nameof(ProceduralImage) + "." + nameof(EncodeAllInfoIntoVertices));
 
@@ -159,7 +159,7 @@ namespace CustomUtils.Runtime.UI.CustomComponents.ProceduralUIImage
 
         private void EncodeAllInfoIntoVertices(VertexHelper vertexHelper)
         {
-#if UNITY_EDITOR
+#if UNITY_EDITOR || DEVELOPMENT_BUILD
             using var encodeVerticesScope = _markerEncodeVertices.Auto();
 #endif
 
@@ -194,9 +194,9 @@ namespace CustomUtils.Runtime.UI.CustomComponents.ProceduralUIImage
             }
         }
 
-        internal ProceduralImageInfo CalculateInfo()
+        private ProceduralImageInfo CalculateInfo()
         {
-#if UNITY_EDITOR
+#if UNITY_EDITOR || DEVELOPMENT_BUILD
             using var calculateInfoScope = _markerCalculateInfo.Auto();
 #endif
 
